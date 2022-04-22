3acaf841cf8b1c896ab6e421cff1f7f01f5fde37
omar khaled <omarkhaled25112001@gmail.com>
2022-04-22 11:22:31 +0200

    Omar khaled

--- Dokan Online Market/Seller.cpp
+++ Dokan Online Market/Seller.cpp
@@ -1,14 +1,16 @@
 #include "Seller.h"
 
-Seller::Seller(int Id, std::string Name, std::string Email, std::string Gender, std::string Phone, std::string Address, std::string Password, std::string Path_photo,Product items []) {
-	this->Id = Id;
+
+Seller::Seller(void) {}
+Seller::~Seller(void) {}
+
+Seller::Seller(int Id, std::string Name, std::string Email, std::string Gender, std::string Phone, std::string Address, std::string Password, std::string Path_photo) {
+	this->ID = Id;
 	this->Name = Name;
-	this->Email = Email;;
+	this->Email = Email;
 	this->Phone = Phone;
 	this->Gender = Gender;
 	this->Address = Address;
 	this->Password = Password;
 	this->Path_photo = Path_photo;
-	//this->items = items;
 }
- 
