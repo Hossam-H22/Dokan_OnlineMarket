84e7e35c7675369d84fb8df0b0b8645aa57aa66e
Hosam Hatem <hosam.hatem222@gmail.com>
2022-04-30 13:53:11 +0200

    Hosam

--- build-DokanOnlineMarket-Desktop_Qt_5_14_2_MSVC2017_64bit-Debug/qtcsettings.cmake
+++
@@ -1,9 +0,0 @@
-# This file is managed by Qt Creator, do not edit!
-
-set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
-set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
-set("CMAKE_PROJECT_INCLUDE_BEFORE" "C:/Qt/Tools/QtCreator/share/qtcreator/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
-set("QT_QMAKE_EXECUTABLE" "C:/Qt/5.14.2/msvc2017_64/bin/qmake.exe" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_PREFIX_PATH" "C:/Qt/5.14.2/msvc2017_64" CACHE "PATH" "" FORCE)
-set("CMAKE_C_COMPILER" "C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Tools/MSVC/14.29.30037/bin/HostX64/x64/cl.exe" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_CXX_COMPILER" "C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Tools/MSVC/14.29.30037/bin/HostX64/x64/cl.exe" CACHE "FILEPATH" "" FORCE)
